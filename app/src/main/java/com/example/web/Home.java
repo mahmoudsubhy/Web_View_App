c5c0e2b9ac58497a6570575954e82212b43a9349
win10 <seamy3@gmail.com>
2020-06-07 02:09:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/web/Home.java
@@ -0,0 +1,38 @@
+package com.example.web;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Home extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        setTitle("الرئيسية");
+
+        mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // REMOTE RESOURCE
+         mWebView.loadUrl("https://google.com");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
